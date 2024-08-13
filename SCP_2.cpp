ae3154f37e4405dc8a64f3e2881a311be650ccb5
MatthewFolino <matthew_folino@proton.me>
2024-08-12 20:14:28 -0400

    Minor updates
    - changed some comments
    - changed the name of a variable
    (counter1 -> curntDigi)
    - removed unnecessary for loop

--- SCP_2.cpp
+++ SCP_2.cpp
@@ -13,31 +13,28 @@ const char charSet[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n
 const int setNumber = 25;            // a constant integer denoting the size of the array of possible characters (compensated for 0 initialization)
 std::vector<int> letterInc = {0};    // a vector to represent the current character in each digit of the password guess
 
-int wordLength = 0;       // integer representing the length of guessWord
+int wordLength = 0;       // integer representing the length of guessWord (0 initialized)
 bool guessMatch = 0;      // bool indicating whether guessWord and passWord match
 int letterNum;            // integer indicating a specific digit of guessWord
+int curntDigi;          // interger used by the ResetLoop function to keep track of which digit of guessword it is operating on
 
 int ResetLoop(int g)      // a loop to reset one or more digits of guessword to the initial value and to add a digit to guessword
 {
         bool x = 0;
-        int counter1 = g;
+        curntDigi = g;
         do{
         if (guessWord[wordLength] == charSet[setNumber])
         {
             break;
         }
-        if(guessWord[counter1] == charSet[setNumber])
+        if(guessWord[curntDigi] == charSet[setNumber])
         {
-            ResetLoop((counter1 + 1));
+            ResetLoop((curntDigi + 1));
         }
 
         else
         {
-            letterInc[counter1]++;
-            for(int i=wordLength; i>=0; i--) //questionably necessary
-            {
-                guessWord[i] = charSet[letterInc[i]];
-            }
+            letterInc[curntDigi]++;
             if (guessWord == passWord)
             {
                 guessMatch = 1;
@@ -46,11 +43,11 @@ int ResetLoop(int g)      // a loop to reset one or more digits of guessword to 
                 return 1;
 
             }
-            for(int i=counter1-1; i>=0; i--)
+            for(int i=curntDigi-1; i>=0; i--)
             {
-                if(guessWord[(counter1 - 1)] == charSet[setNumber])
+                if(guessWord[(curntDigi - 1)] == charSet[setNumber])
                 {
-                    letterInc[(counter1 - 1)] = 0;
+                    letterInc[(curntDigi - 1)] = 0;
                     if (guessWord == passWord)
                     {
                         guessMatch = 1;
